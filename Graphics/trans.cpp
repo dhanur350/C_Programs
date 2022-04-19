91fa14d53de080faca0c5028ee0bcd597eed3211
dhanur350 <dhanur297@gmail.com>
2022-04-19 07:28:35 +0530

    Day1.1

--- Graphics/trans.cpp
+++ Graphics/trans.cpp
@@ -1,10 +1,11 @@
 #include <graphics.h>
+#include <math.h>
 int main()
 {
     int graphd = DETECT, graphm;
     int tx = 200, ty = 200, clr;
-    float sx = 1.8, sy = 1.8, rad, d;
-    static int ii, jj; // by default 0 value
+    float sx = 1.8, sy = 1.8, rad, d = 15;
+    static int ii, jj, xr = 150, yr = 150; // by default 0 value
     rad = d * 3.142 / 180;
     initgraph(&graphd, &graphm, NULL);
     setbkcolor(GREEN);
@@ -18,12 +19,16 @@ int main()
         {
             // clr = getpixel(i, j);
             //  printf("%d", &clr);
-            ii = (int)(i * sx);
-            jj = (int)(j * sy);
+            // ii = (int)(i * cos(rad) - j * sin(rad));
+            // jj = (int)(j * sin(rad) + j * cos(rad));
+            ii = (int)(xr + (i - xr) * cos(rad) - (j - yr) * sin(rad));
+            jj = (int)(yr + (j - yr) * sin(rad) - (j - yr) * cos(rad));
+            // ii = (int)(i * sx);
+            // jj = (int)(j * sy);
             // ii = i + tx;
             // jj = j + ty;
             putpixel(ii, jj, getpixel(i, j));
-            putpixel(i, j, GREEN);
+            // putpixel(i, j, GREEN);
         }
         printf(" ");
     }
