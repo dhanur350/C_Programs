6fd3923a29d6eb9fbcd8651de4fb9b01be5f0f10
dhanur350 <dhanur297@gmail.com>
2022-09-15 21:52:44 +0530

    Patch2

--- PCB/process.cpp
+++ PCB/process.cpp
@@ -22,21 +22,26 @@ void priority(){
     //Initialized array of vectors, this'll store string dynamically
     vector<string> processes;
     cout<<"Process priority of processes came in CPU Process Control Block"<<endl;
+    cout<<"Enter your processes only 4 allowed\n";
     for(int i=0;i<4;i++){
         char arr[10];
         cin>>arr;
         processes.push_back(arr);
+        /**
+         * @brief this push_back() will add strings as name of processes
+         * 
+         * */
     }
     cout<<"\n\nProcesses Priority as accoriding to CPU PCB is\n"<<endl;
     for(int i = 0;i < processes.size();i++){
-        cout<<"No. "<<rand()<<" "<<processes[i]<<endl;
+        cout<<"No. "<<rand()<<" "<<processes[i]<<endl; //rand() will gegerate random numbers for process scheduling CPU for PCB
     }
 }
 int main(void){
     /**
      * @brief Construct a new fcfs object use it according to your need
-     * 
-    fcfs();
+     * uncomment one function or both according to your need
+    fcfs(); 
     priority();
      */
     
