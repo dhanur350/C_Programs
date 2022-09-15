e4604413a45336c4bb6466ecabf3f074a0a1c499
dhanur350 <dhanur297@gmail.com>
2022-09-15 21:40:38 +0530

    Patch1

--- PCB/process.cpp
+++ PCB/process.cpp
@@ -2,9 +2,10 @@
 #include<vector>
 //Vector is used to store arrays of strings or number dynamically
 using namespace std;
-int main(void){
+
+void fcfs(){
     cout<<"Process PCB Simulation"<<endl;
-    cout<<"Enter your process names, only 4 processes are allowed\nAccording to our Simulation Example";
+    cout<<"Enter your process names, only 4 processes are allowed\nAccording to our Simulation Example"<<endl;
     //Initialized array of vectors, this'll store string dynamically
     vector<string> processes;
     for(int i=0;i<4;i++){
@@ -12,8 +13,19 @@ int main(void){
         cin>>arr;
         processes.push_back(arr);
     }
-    cout<<"Processes Priority was before scheduled in PCB as in FCFS\ni.e. (First come First serve)"<<endl;
+    cout<<"\n\nProcesses Priority was before scheduled in PCB as in FCFS\ni.e. (First come First serve)\n"<<endl;
     for(int i = 0;i < processes.size();i++){
-        cout<<"Processes Priority "<<processes[i]<<endl;
+        cout<<"No. "<<i<<" "<<processes[i]<<endl;
     }
+}
+void priority(){
+    cout<<"Process priority ";
+}
+int main(void){
+    /**
+     * @brief Construct a new fcfs object use it according to your need
+     * 
+    fcfs();
+    priority();
+     */
 }
